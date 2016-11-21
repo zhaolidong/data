7c569f1c8b6139b95ea07a5715671b170e99ba92
zld <zld>
2016-11-21 14:55:09 +0800

    注册微信号
    上传注册微信号页面

---
+++ wxregister/js/add.js
@@ -0,0 +1,12 @@
+jQuery(function($) {
+	function add() {
+		var html = document.documentElement;
+		var hei = html.clientWidth;
+		var fz = hei / 375 * 20 + "px";
+		html.style.fontSize = fz;
+	};
+	add();
+	window.addEventListener("resize", add, false);
+	window.onload = add();
+})
+
